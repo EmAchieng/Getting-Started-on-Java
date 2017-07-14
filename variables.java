a67d657ebbcead9cd244069f29710041f62528bc
Emily Achieng <emillyachieng@gmail.com>
2017-07-14 17:02:07 +0900

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,31 @@
+public class variables {
+	public static void main(String[] args) {
+
+
+     int anythingIWant;//Declaration
+     anythingIWant =5;// Initialization
+
+     
+	System.out.println(anythingIWant);
+
+     //double quotes for strings
+     String monkey = "bananas";
+     System.out.println(monkey);
+     
+	//don't forget to comment
+     //think of texts when you think of strings
+
+     boolean isTrue = true;
+     int number = 5;
+     float decimalNumber = 0.5f;
+     double biggerDecimalNumber = 0.5d;
+
+     //single quotes for characters
+     char letter = 'a';
+     byte tinyData = 0b0000001;
+     
+
+    
+
+	}
+}
