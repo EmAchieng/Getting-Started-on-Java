5bc770ceee3816bba760fa1655e1bd499c817ae5
Emily Achieng <emillyachieng@gmail.com>
2017-07-14 17:01:43 +0900

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,28 @@
+// Methods and Functions are almost same in meaning
+public class Methods {
+	public static void main(String[] args) {
+		System.out.println("Happy!");
+       // add was on the function
+		//when you call a function, you call it by it's name
+		//name is add
+         //function call
+
+	int mySubtract = difference (5,2);
+     System.out.println("mySubtract:" + mySubtract);
+	}
+
+	//we can define our own method here
+
+	//Function Signature
+	public static int difference (int a, int b)
+ {
+
+ 	//Function body
+ 	// return finishes the function
+
+  return a - b;
+
+ }
+
+
+}
